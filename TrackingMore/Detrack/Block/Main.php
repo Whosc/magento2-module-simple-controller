f607a9ab2339eb734ccc2a61d859eb0f57951434
whao <1421851519@qq.com>
2016-07-21 13:44:27 +0800

    project init

---
+++ TrackingMore/Detrack/Block/Main.php
@@ -0,0 +1,11 @@
+<?php
+namespace TrackingMore\Detrack\Block;
+use Magento\Framework\View\Element\Template;
+
+class Main extends Template
+{    
+    protected function _prepareLayout()
+    {
+    
+    }
+}
---
+++ TrackingMore/Detrack/Controller/Hello/World.php
@@ -0,0 +1,22 @@
+<?php
+namespace TrackingMore\Detrack\Controller\Hello;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Framework\App\Action\Context;
+
+class World extends \Magento\Framework\App\Action\Action
+{
+    protected $pageFactory;
+    public function __construct(Context $context, PageFactory $pageFactory)
+    {
+        $this->pageFactory = $pageFactory;
+        return parent::__construct($context);
+    }
+
+    public function execute()
+    {      
+        print_r(__METHOD__);
+		$a = 1111;
+        $page_object = $this->pageFactory->create();;
+        return $page_object;
+    }    
+}
